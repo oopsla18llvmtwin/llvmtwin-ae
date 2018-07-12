c2eba3bd45e88388622bb8ad0d554217ff6ea4b7
oopsla18llvmtwin <oopsla18llvmtwin>
2018-07-12 17:24:59 +0000

    Update Rust scripts

---
+++ miscompilations/rustbug/run-base.sh
@@ -0,0 +1,9 @@
+rm -rf target
+echo $PATH
+export PATH=/home/oopsla18/llvmtwin-ae/rust-base-install/bin:$PATH
+echo "Correct output: c = 0, x = 7777 and c = 1, x = 42."
+echo not optimized:
+cargo run
+echo
+echo optimized:
+cargo run --release
--- miscompilations/rustbug/run-patched.sh
+++ miscompilations/rustbug/run-patched.sh
@@ -1,3 +1,6 @@
+rm -rf target
+echo $PATH
+export PATH=/home/oopsla18/llvmtwin-ae/rust-patched-install/bin:$PATH
 echo "Correct output: c = 0, x = 7777 and c = 1, x = 42."
 echo not optimized:
 cargo run
